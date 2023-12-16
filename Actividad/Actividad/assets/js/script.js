3a3b5796b3adef273f61fbe35f0ba79e727046fc
jessebelalbornoz <jimenezjessebel777.com>
2023-12-16 09:48:02 -0500

    repositorio de la unidas 2

---
+++ Actividad/Actividad/assets/js/script.js
@@ -0,0 +1,25 @@
+var edad = 20;
+console.log(edad);
+
+
+
+var estudioHoy = true;
+console.log(estudioHoy);
+
+var miestatura = 1.55;
+console.log(miestatura);
+
+var x = 7;
+var y = 8;
+//esto es una suma
+var suma = x + y;
+console.log(suma);
+
+console.log(5*6);
+console.log(10==="10");
+console.log(10 !== "10");
+console.log(6>5 || 10<2);
+
+var nombres = document.getElementById("nombres").value;
+console.log(nombres);
+
---
+++ js/script.js
@@ -0,0 +1,66 @@
+console.log("hola mundo");
+ console.log("Mi nombre es Jesse");
+ console.log("ADIOSITO");
+
+ var MItitulo = "Mi Primera Pagina"
+
+ console.log(MItitulo)
+
+ document.getElementById("title")
+
+ var miEtiquetaTitulo = document,getElementById
+
+console.log(miEtiquetaTitulo)
+
+
+var edad = 20;
+console.log(edad);
+
+
+
+var estudioHoy = true;
+console.log(estudioHoy);
+
+var miestatura = 1.55;
+console.log(miestatura);
+
+//console.log("hola mundo");
+
+/*var num1 = prompt("ingrese el primero número: ");
+var num2 = prompt("ingrese el segundo número: ");
+
+var num1 = parseInt(num1texto);
+var num2 = parseInt(num2texto);
+
+console.log(typeof(num1));
+console.log(num1);
+console.log(num2);
+
+var suma = num1 + num2;
+console.log("Suma: ",suma);
+
+var resta = num1 - num2;
+console.log("Esta es la resta->", resta);
+
+var multiplicación = num1 * num2;
+console.log("multiplicación: ", multiplicación);
+
+var división = num1 / num2;
+console.log("división: ", división);
+
+var residuo = num1 % num2;
+console.log("residuo: ", residuo);
+
+var exponenciación = num1 ** num2;
+console.log("exponenciación: ", exponenciación);*/
+
+//PRACTICANDO OPERACIONES CON STRING
+
+var mitexto = "PRACTICANDO OPERACIONES CON STRING. \n\PRACTICANDO OPERACIONES CON STRING.";
+console.log(mitexto);
+
+var txt = "ABCDEFGHIJKMNOPQRSTUVWXYZ"
+var len = txt.length
+
+
+
